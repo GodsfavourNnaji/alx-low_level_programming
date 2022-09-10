36efc375e150fe8d1cdbf704d38b9e4199f052f2
Godsfavour <godsfavournnaji@gmail.com>
2022-09-09 19:35:19 -0700

    Scripts 1 to 4 added

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <studio.h>
+
+/**
+ * main - prints the phrase "Programming is like building a multilingual puzzle"
+ */
+int main(void)
+{
+		puts("\"Programming is like building a multilingual puzzle");
+		return (0);
+}
