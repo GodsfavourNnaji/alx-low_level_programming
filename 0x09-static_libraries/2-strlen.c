b1ec6c9a730d28b8841e3ca1cb78d883b6a03058
Godsfavour <godsfavournnaji@gmail.com>
2022-10-04 21:56:36 -0700

    Static library created

--- 0x09-static_libraries/2-strlen.c
+++ 0x09-static_libraries/2-strlen.c
@@ -3,17 +3,15 @@
 /**
  * _strlen - returns the length of a string
  * @s: string
+ *
  * Return: length of the string
  */
 int _strlen(char *s)
 {
 	int len = 0;
 
-	while (*s != '\0')
-	{
+	while (*s++)
 		len++;
-		s++;
-	}
 
 	return (len);
 }
